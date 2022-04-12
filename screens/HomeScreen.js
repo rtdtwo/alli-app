43fa1783fc6448a21d3992bc300976a7a3a1752c
Rishabh Tatiraju <me@rishabh.blog>
2022-04-11 21:32:24 -0400

    Added code for API calls and Local Storage

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,6 +1,28 @@
 import { View } from "react-native"
+import { useEffect, useState } from "react/cjs/react.production.min"
+import { getCurrentUser } from '../storage/storage'
+
+import API from '../network/api'
 
 const HomeScreen = () => {
+
+    const [goals, setGoals] = useState([])
+    const [currentUser, setCurrentUser] = useState(null)
+
+    useEffect(() => {
+        getCurrentUser()
+            .then(user => setCurrentUser(user))
+            .catch(e => {
+                console.log(e)
+                setCurrentUser(null)
+            })
+    })
+
+    useEffect(() => {
+        API.getAllGoalsOfUser()
+        .then(response => setGoals(response.data))
+        .catch(error => console.log(`Error Occurred - ${error.code}: ${error.msg}`))
+    }, [])
     return <View>
     </View>
 }
